8d65a57804b7497a38dfb058f48125c1c5f055f2
dittoyang0226 <sysdittoyang@gmail.com>
2024-05-14 13:12:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -36,14 +36,8 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     'dittoyang'
-    // '🔍 分享与热心帮助',
-    // '🤝 专修交互与设计',
-    // '🏃 脚踏实地行动派',
-    // '🏠 智能家居小能手',
-    // '🤖️ 数码科技爱好者',
-    // '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/dittoyang0226', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
