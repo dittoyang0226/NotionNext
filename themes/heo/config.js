12a969342945f4e0d69d30f17adcd90e6c12506e
dittoyang0226 <sysdittoyang@gmail.com>
2024-05-14 13:29:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  // HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  // HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  // HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -35,13 +35,12 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    'dittoyang'
-    // '🔍 分享与热心帮助',
-    // '🤝 专修交互与设计',
-    // '🏃 脚踏实地行动派',
-    // '🏠 智能家居小能手',
-    // '🤖️ 数码科技爱好者',
-    // '🧱 团队小组发动机'
+    '🔍 分享与热心帮助',
+    '🤝 专修交互与设计',
+    '🏃 脚踏实地行动派',
+    '🏠 智能家居小能手',
+    '🤖️ 数码科技爱好者',
+    '🧱 团队小组发动机'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
