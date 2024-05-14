295295e9c18349f5894dc34e8513e801e00ef95b
dittoyang0226 <sysdittoyang@gmail.com>
2024-05-14 16:49:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://github.com/dittoyang0226',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '', url: '' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/python' },
   HEO_HERO_CATEGORY_2: { title: '', url: '' },
   HEO_HERO_CATEGORY_3: { title: '', url: '' },
 
