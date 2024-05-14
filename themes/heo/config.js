e9360bb26b4dfc03bd17e9a3a86377407edd0d8e
dittoyang0226 <sysdittoyang@gmail.com>
2024-05-14 13:27:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,10 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [],
+  HEO_NOTICE_BAR: [
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -20,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1:{},
-  HEO_HERO_CATEGORY_2: {},
-  HEO_HERO_CATEGORY_3:{},
+  // HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  // HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  // HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -33,8 +36,14 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     'dittoyang'
+    // '🔍 分享与热心帮助',
+    // '🤝 专修交互与设计',
+    // '🏃 脚踏实地行动派',
+    // '🏠 智能家居小能手',
+    // '🤖️ 数码科技爱好者',
+    // '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/dittoyang0226', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
