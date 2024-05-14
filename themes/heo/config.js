b028658ef35790d089a17e6df9b54d06594f8bf5
dittoyang0226 <sysdittoyang@gmail.com>
2024-05-14 13:21:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,6 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    // { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -23,9 +21,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  // HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  // HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  // HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1:{},
+  HEO_HERO_CATEGORY_2: {},
+  HEO_HERO_CATEGORY_3:{},
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
